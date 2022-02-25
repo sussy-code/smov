fad23f60b18a32cbb5af863bc534c7e30e0696c3
mrjvs <jellevs@gmail.com>
2022-02-25 21:50:36 +0100

    top navigation

---
+++ src/components/layout/BrandHeader.tsx
@@ -0,0 +1,18 @@
+import { IconPatch } from "components/buttons/IconPatch";
+import { Icons } from "components/Icon";
+import { DISCORD_LINK, GITHUB_LINK } from "mw_constants";
+import { BrandPill } from "./BrandPill";
+
+export function BrandHeader() {
+  return (
+    <div className="flex justify-between items-center absolute left-0 right-0 top-0 p-3">
+      <div>
+        <BrandPill />
+      </div>
+      <div className="flex">
+        <a href={DISCORD_LINK} target="_blank" className="text-2xl text-white"><IconPatch icon={Icons.DISCORD} clickable/></a>
+        <a href={GITHUB_LINK} target="_blank" className="text-2xl text-white"><IconPatch icon={Icons.GITHUB} clickable/></a>
+      </div>
+    </div>
+  )
+}
