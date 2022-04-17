6a737396c3d1ec8c07180e625fa4a0531e45de62
James Hawkins <jhawki2005@gmail.com>
2022-04-17 13:36:33 +0100

    Add gdriveplayer source
    This source is temperamental at best, and captions no longer work. We should remove it when we have some more reliable options.

--- src/providers/methods/providers.ts
+++ src/providers/methods/providers.ts
@@ -1,8 +1,10 @@
 import { theFlixScraper } from "providers/list/theflix";
+import { gDrivePlayerScraper } from "providers/list/gdriveplayer";
 import { MWWrappedMediaProvider, WrapProvider } from "providers/wrapper";
 
 export const mediaProvidersUnchecked: MWWrappedMediaProvider[] = [
   WrapProvider(theFlixScraper),
+  WrapProvider(gDrivePlayerScraper),
 ];
 
 export const mediaProviders: MWWrappedMediaProvider[] =
