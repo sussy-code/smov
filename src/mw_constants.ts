c9d3addacd795cc3edf40f7a1d8ce65d2f04d166
James Hawkins <jhawki2005@gmail.com>
2022-12-18 18:12:39 +0000

    Update mw_constants.ts

--- src/mw_constants.ts
+++ src/mw_constants.ts
@@ -4,4 +4,4 @@ export const TMDB_API_KEY = "b030404650f279792a8d3287232358e3";
 export const OMDB_API_KEY = "aa0937c0";
 export const DISCORD_LINK = "https://discord.gg/Jhqt4Xzpfb";
 export const GITHUB_LINK = "https://github.com/JamesHawkinss/movie-web";
-export const APP_VERSION = "2.0.5";
+export const APP_VERSION = "2.1.0";
