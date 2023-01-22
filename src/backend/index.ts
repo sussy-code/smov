fa9785bf69617736d64d112be2f2d5b482891bda
Jip Fr <jipfrijlink@gmail.com>
2023-01-22 23:06:29 +0100

    chore(superstream): improve import, move quality finding to its own function

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,7 +3,7 @@ import { initializeScraperStore } from "./helpers/register";
 // providers
 import "./providers/gdriveplayer";
 import "./providers/flixhq";
-import "./providers/superstream/superstream";
+import "./providers/superstream";
 
 // embeds
 // -- nothing here yet
