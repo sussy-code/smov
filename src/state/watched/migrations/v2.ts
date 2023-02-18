ad518a6508cce904c998c23e490473255c5d602b
mrjvs <jellevs@gmail.com>
2023-02-18 14:03:48 +0100

    more fuzzy matching for migrations
    Co-authored-by: Jip Frijlink <JipFr@users.noreply.github.com>

--- src/state/watched/migrations/v2.ts
+++ src/state/watched/migrations/v2.ts
@@ -1,6 +1,7 @@
 import { DetailedMeta, getMetaFromId } from "@/backend/metadata/getmeta";
 import { searchForMedia } from "@/backend/metadata/search";
 import { MWMediaMeta, MWMediaType } from "@/backend/metadata/types";
+import { compareTitle } from "@/utils/titleMatch";
 import { WatchedStoreData, WatchedStoreItem } from "../types";
 
 interface OldMediaBase {
@@ -49,10 +50,10 @@ async function getMetas(
         type: item.mediaType,
       });
       const relevantItem = data.find((res) =>
-        yearsAreClose(Number(res.year), year)
+        yearsAreClose(Number(res.year), year) && compareTitle(res.title, item.title)
       );
       if (!relevantItem) {
-        console.error("No item");
+        console.error("No item found for migration: " + item.title);
         return;
       }
       return {
