02cd565f84be6ee3882ca843deb8166b6a9d8983
mrjvs <jellevs@gmail.com>
2023-02-21 21:51:57 +0100

    version bump

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,4 +1,4 @@
 export const DISCORD_LINK = "https://discord.gg/Jhqt4Xzpfb";
 export const GITHUB_LINK = "https://github.com/movie-web/movie-web";
-export const APP_VERSION = "3.0.2";
+export const APP_VERSION = "3.0.3";
 export const GA_ID = "G-44YVXRL61C";
