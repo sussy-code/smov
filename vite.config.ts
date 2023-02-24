28c008a77f249925bcc2fd351452d9729de9aec0
mrjvs <jellevs@gmail.com>
2023-02-24 22:20:35 +0100

    add any purpose

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,18 @@ export default defineConfig({
             src: "android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
             purpose: "maskable",
           },
           {
