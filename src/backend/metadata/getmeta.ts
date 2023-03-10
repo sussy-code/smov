30cc5aa78b40b53ea12a1d3835cc113e5cf75960
mrjvs <jellevs@gmail.com>
2023-03-10 20:59:10 +0100

    fix more linting

--- src/backend/metadata/getmeta.ts
+++ src/backend/metadata/getmeta.ts
@@ -56,19 +56,15 @@ export async function getMetaFromId(
 
   let imdbId = data.external_ids.find(
     (v) => v.provider === "imdb_latest"
-  )?.external_id
+  )?.external_id;
   if (!imdbId)
-    imdbId = data.external_ids.find(
-      (v) => v.provider === "imdb"
-    )?.external_id;
+    imdbId = data.external_ids.find((v) => v.provider === "imdb")?.external_id;
 
   let tmdbId = data.external_ids.find(
     (v) => v.provider === "tmdb_latest"
-  )?.external_id
+  )?.external_id;
   if (!tmdbId)
-  tmdbId = data.external_ids.find(
-      (v) => v.provider === "tmdb"
-    )?.external_id;
+    tmdbId = data.external_ids.find((v) => v.provider === "tmdb")?.external_id;
 
   if (!imdbId || !tmdbId) throw new Error("not enough info");
 
