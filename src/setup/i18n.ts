aa91bae4183dc929743b05b3c547afa0f21eb62a
panmlg <80635767+panmlg@users.noreply.github.com>
2023-04-25 16:59:19 +0200

    fix of broken thingy

--- src/setup/i18n.ts
+++ src/setup/i18n.ts
@@ -4,11 +4,11 @@ import { initReactI18next } from "react-i18next";
 
 // Languages
 import { captionLanguages } from "./iso6391";
+import cs from "./locales/cs/translation.json";
 import en from "./locales/en/translation.json";
 import fr from "./locales/fr/translation.json";
 import nl from "./locales/nl/translation.json";
 import tr from "./locales/tr/translation.json";
-import cs from "./locales/cs/translation.json";
 
 const locales = {
   en: {
@@ -25,7 +25,7 @@ const locales = {
   },
   cs: {
     translation: cs,
-  }
+  },
 };
 i18n
   // detect user language
