cd3bd22a2ccf190af2ef87a09d22944fae826abc
James Hawkins <jhawki2005@gmail.com>
2023-05-06 10:24:57 +0100

    Update i18n.ts

--- src/setup/i18n.ts
+++ src/setup/i18n.ts
@@ -4,8 +4,8 @@ import { initReactI18next } from "react-i18next";
 
 // Languages
 import { captionLanguages } from "./iso6391";
-import de from "./locales/de/translation.json";
 import cs from "./locales/cs/translation.json";
+import de from "./locales/de/translation.json";
 import en from "./locales/en/translation.json";
 import fr from "./locales/fr/translation.json";
 import nl from "./locales/nl/translation.json";
