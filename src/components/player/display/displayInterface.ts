ecc7834f44149b84d10b6c22249aaebda9fa56a5
mrjvs <jellevs@gmail.com>
2023-09-01 17:47:09 +0200

    remove controller, introduce display interfaces

--- src/components/player/display/displayInterface.ts
+++ src/components/player/display/displayInterface.ts
@@ -1,21 +1,12 @@
 import { Source } from "@/components/player/hooks/usePlayer";
+import { Listener } from "@/utils/events";
 
-type EventMap = Record<string, any>;
-type EventKey<T extends EventMap> = string & keyof T;
-type EventReceiver<T> = (params: T) => void;
+export type DisplayInterfaceEvents = {
+  play: void;
+  pause: void;
+};
 
-export interface Emitter<T extends EventMap> {
-  on<K extends EventKey<T>>(eventName: K, fn: EventReceiver<T[K]>): void;
-  off<K extends EventKey<T>>(eventName: K, fn: EventReceiver<T[K]>): void;
-  emit<K extends EventKey<T>>(eventName: K, params: T[K]): void;
-}
-
-interface Listener<T extends EventMap> {
-  on<K extends EventKey<T>>(eventName: K, fn: EventReceiver<T[K]>): void;
-}
-
-export interface DisplayInterface<Events extends EventMap>
-  extends Listener<Events> {
+export interface DisplayInterface extends Listener<DisplayInterfaceEvents> {
   play(): void;
   pause(): void;
   load(source: Source): void;
--- src/stores/player/controllers/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Controller {
-  pause(): void;
-  play(): void;
-  setVolume(target: number): void;
-  registerVideoElement(videoElement: HTMLVideoElement): void;
-}
