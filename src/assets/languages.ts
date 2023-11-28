b10cf309534f48303d7a2603e114116ae196fb28
mrjvs <jellevs@gmail.com>
2023-11-28 22:02:06 +0100

    register new languages and remove old

--- src/assets/languages.ts
+++ src/assets/languages.ts
@@ -1,5 +1,23 @@
+import cs from "@/assets/locales/cs.json";
+import de from "@/assets/locales/de.json";
 import en from "@/assets/locales/en.json";
+import fr from "@/assets/locales/fr.json";
+import it from "@/assets/locales/it.json";
+import nl from "@/assets/locales/nl.json";
+import pl from "@/assets/locales/pl.json";
+import tr from "@/assets/locales/tr.json";
+import vi from "@/assets/locales/vi.json";
+import zh from "@/assets/locales/zh.json";
 
 export const locales = {
   en,
+  cs,
+  de,
+  fr,
+  it,
+  nl,
+  pl,
+  tr,
+  vi,
+  zh,
 };
