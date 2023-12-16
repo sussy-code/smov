e3807d31c1737d94e914dfbb1255ec746f70b6e5
Jip Fr <jipfrijlink@gmail.com>
2023-12-16 01:48:26 +0100

    Add minionlang

--- src/components/FlagIcon.tsx
+++ src/components/FlagIcon.tsx
@@ -35,6 +35,17 @@ export function FlagIcon(props: FlagIconProps) {
       </div>
     );
 
+  if (countryCode === "minion")
+    return (
+      <div className="w-8 h-6 rounded bg-[#ffff1a] flex justify-center items-center">
+        <div className="w-4 h-4 border-2 border-gray-500 rounded-full bg-white flex justify-center items-center">
+          <div className="w-1.5 h-1.5 rounded-full bg-gray-900 relative">
+            <div className="absolute top-0 left-0 w-1 h-1 bg-white rounded-full transform -translate-x-1/3 -translate-y-1/3" />
+          </div>
+        </div>
+      </div>
+    );
+
   return (
     <span
       className={classNames(
