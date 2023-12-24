2e0a5910ca9edefd1cc8d386243fd8c0de7125d5
mrjvs <jellevs@gmail.com>
2023-12-24 16:12:28 +0100

    Fix missing timeout on touch controls hovering

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
     "no-nested-ternary": "off",
     "prefer-destructuring": "off",
     "no-param-reassign": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern:  "^_" }],
     "react/jsx-filename-extension": [
       "error",
       { extensions: [".js", ".tsx", ".jsx"] }
