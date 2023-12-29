7265b34459c0a5368f7a71edb17a01ee0517d539
mrjvs <jellevs@gmail.com>
2023-12-29 22:39:33 +0100

    Add languages to language selector

--- src/assets/languages.ts
+++ src/assets/languages.ts
@@ -6,6 +6,7 @@ import es from "@/assets/locales/es.json";
 import et from "@/assets/locales/et.json";
 import fr from "@/assets/locales/fr.json";
 import he from "@/assets/locales/he.json";
+import hi from "@/assets/locales/hi.json";
 import it from "@/assets/locales/it.json";
 import lv from "@/assets/locales/lv.json";
 import minion from "@/assets/locales/minion.json";
@@ -13,10 +14,12 @@ import ne from "@/assets/locales/ne.json";
 import nl from "@/assets/locales/nl.json";
 import pirate from "@/assets/locales/pirate.json";
 import pl from "@/assets/locales/pl.json";
+import ptbr from "@/assets/locales/pt-BR.json";
 import sv from "@/assets/locales/sv.json";
 import th from "@/assets/locales/th.json";
 import tok from "@/assets/locales/tok.json";
 import tr from "@/assets/locales/tr.json";
+import uk from "@/assets/locales/uk.json";
 import vi from "@/assets/locales/vi.json";
 import zh from "@/assets/locales/zh.json";
 
@@ -42,6 +45,9 @@ export const locales = {
   es,
   et,
   tok,
+  hi,
+  pt: ptbr,
+  uk,
 };
 export type Locales = keyof typeof locales;
 
