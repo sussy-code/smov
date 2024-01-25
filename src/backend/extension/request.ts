b5cd297ad88e4a9f8c1ad97045b1eaebef310e2d
Jorrin <43169049+JorrinKievit@users.noreply.github.com>
2024-01-25 19:37:15 +0100

    mr feedback

--- src/backend/extension/request.ts
+++ src/backend/extension/request.ts
@@ -6,17 +6,12 @@ export function getBodyTypeFromBody(
   if (typeof body === "string") return "string";
   if (body instanceof FormData) return "FormData";
   if (body instanceof URLSearchParams) return "URLSearchParams";
-  if (typeof body === "object") return "object";
-  return undefined;
+  return "object";
 }
 
 export function convertBodyToObject(body: unknown): any {
   if (body instanceof FormData || body instanceof URLSearchParams) {
-    const obj: Record<string, any> = {};
-    for (const [key, value] of body.entries()) {
-      obj[key] = value;
-    }
-    return obj;
+    return [...body];
   }
   return body;
 }
