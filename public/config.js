380af12138a62dbf815fd7c21f53ccb949d8dbd8
Cooper Ransom <cooperransom08@outlook.com>
2024-02-26 15:38:52 -0500

    Migrate proxy URL to new proxy

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "https://flixweb-proxy.netlify.app",
+  VITE_CORS_PROXY_URL: "https://sudo-proxy.netlify.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZmU0OGY4NjFkY2NmMjczMzUyMDdmMWVjYmVkNjNjNiIsInN1YiI6IjY1YjNmMWI0NTk0Yzk0MDE2MzNkZDBjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GiCKswc2u9NraBbujm0ykI5G3p-K9WJoHg40jYbFv4o",
@@ -12,7 +12,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: false,
 
   // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
-  VITE_BACKEND_URL: "https://sudo-flix-backend.up.railway.app",
+  VITE_BACKEND_URL: "https://sudo-flix-backend.up.railway.app", // Host on pro account before this DB fills up
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: ""
--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "https://discord.gg/GGt2deXzUa";
-export const GITHUB_LINK = "https://github.com/movie-web/movie-web";
+export const GITHUB_LINK = "https://github.com/movie-web/movie-web"; // https://gitlab.com/itzCozi/sudo-flix
 export const DONATION_LINK = "https://ko-fi.com/movieweb";
 export const GA_ID = "G-44YVXRL61C";
-export const BACKEND_URL = "https://backend.movie-web.app";
+export const BACKEND_URL = "https://sudo-flix-backend.up.railway.app"; // https://backend.movie-web.app
