7159b76344948ef9b98e83013ae0c39beea0f95a
Isra <byzkk@protonmail.com>
2024-03-06 10:58:38 -0600

    Add onboarding styles to default theme

--- themes/default.ts
+++ themes/default.ts
@@ -228,6 +228,24 @@ export const defaultTheme = {
         },
       },
 
+      // Utilities
+      utils: {
+        divider: tokens.ash.c300,
+      },
+
+      // Onboarding
+      onboarding: {
+        bar: tokens.shade.c400,
+        barFilled: tokens.purple.c300,
+        divider: tokens.shade.c200,
+        card: tokens.shade.c800,
+        cardHover: tokens.shade.c700,
+        border: tokens.shade.c600,
+        good: tokens.purple.c100,
+        best: tokens.semantic.yellow.c100,
+        link: tokens.purple.c100,
+      },
+
       utils: {
         divider: tokens.ash.c300,
       },
