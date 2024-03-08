94614e69ac0d3f79671d27aa13e4f43aea0cf60f
Cooper Ransom <cooperransom08@outlook.com>
2024-03-07 20:35:41 -0500

    Give up on turnstile for now

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: ["https://sudo-proxy2.netlify.app", "https://sudo-proxy3.up.railway.app"],
+  VITE_CORS_PROXY_URL: "https://sudo-proxy.netlify.app,https://sudo-proxy1.up.railway.app,https://sudo-proxy2.up.railway.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTljNGE2ZDE1ZDFiODZiNzdlMWQyYmI5ZGY0MzdmYyIsInN1YiI6IjY1YjNmMWI0NTk0Yzk0MDE2MzNkZDBjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kAX7TkbKuJkNty6IsjcCLnoENFicVZn6d6DkLQsy3p8",
@@ -14,7 +14,8 @@ window.__CONFIG__ = {
   // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
   VITE_BACKEND_URL: "https://sudo-flix-backend.lol", // "https://mw-backend.lonelil.com",
 
-  //VITE_TURNSTILE_KEY: "0x4AAAAAAATd-oefbvXlJvmj", // "3x00000000000000000000FF",
+  // Maybe do this at a later date
+  //VITE_TURNSTILE_KEY: "3x00000000000000000000FF",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: "",
