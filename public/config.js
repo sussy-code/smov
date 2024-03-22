15e11d30aad700307260b8efd571bb1c13a908e6
Cooper Ransom <cooperransom08@outlook.com>
2024-03-22 13:31:25 -0400

    Add custom @sudo-flix email

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.CONFIG = {
   VITE_BACKEND_URL: "https://backend.sudo-flix.lol/",
   VITE_CORS_PROXY_URL: "https://sudo-proxy1.sudo-flix.lol%2Chttps//sudo-proxy2.sudo-flix.lol,https://sudo-proxy3.sudo-flix.lol,https://sudo-proxy4.sudo-flix.lol,https://sudo-proxy5.sudo-flix.lol",
-  VITE_DMCA_EMAIL: "sudo-flix@proton.me",
+  VITE_DMCA_EMAIL: "dev@sudo-flix.lol",
   VITE_GA_ID: "G-3VB2TNCW2V",
   VITE_NORMAL_ROUTER: true,
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTljNGE2ZDE1ZDFiODZiNzdlMWQyYmI5ZGY0MzdmYyIsInN1YiI6IjY1YjNmMWI0NTk0Yzk0MDE2MzNkZDBjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kAX7TkbKuJkNty6IsjcCLnoENFicVZn6d6DkLQsy3p8",
