b7be0163773968bce546a1bd20fcc9dfe15cbf01
Cooper Ransom <cooperransom08@outlook.com>
2024-03-26 19:43:53 -0400

    Add better scrape error dialog

--- src/pages/parts/player/ScrapeErrorPart.tsx
+++ src/pages/parts/player/ScrapeErrorPart.tsx
@@ -3,7 +3,7 @@ import { Trans, useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
 
 import { isAllowedExtensionVersion } from "@/backend/extension/compatibility";
-import { extensionInfo } from "@/backend/extension/messaging";
+import { extensionInfo, sendPage } from "@/backend/extension/messaging";
 import { Button } from "@/components/buttons/Button";
 import { Icons } from "@/components/Icon";
 import { IconPill } from "@/components/layout/IconPill";
@@ -47,6 +47,8 @@ export function ScrapeErrorPart(props: ScrapeErrorPartProps) {
   const location = useLocation();
   const [extensionState, setExtensionState] =
     useState<ExtensionStatus>("unknown");
+  const [title, setTitle] = useState(t("player.scraping.notFound.title"));
+  const [icon, setIcon] = useState(Icons.WAND);
 
   const error = useMemo(() => {
     const data = props.data;
@@ -65,20 +67,24 @@ export function ScrapeErrorPart(props: ScrapeErrorPartProps) {
   }, [props, location]);
 
   useEffect(() => {
-    getExtensionState().then(setExtensionState);
-  }, []);
+    getExtensionState().then((state) => {
+      setExtensionState(state);
+      if (state === "disallowed") {
+        setTitle(t("player.scraping.extensionFailure.disabledTitle"));
+        setIcon(Icons.LOCK);
+      }
+    });
+  }, [t]);
 
   return (
     <ErrorLayout>
       <ErrorContainer>
-        <IconPill icon={Icons.WAND}>
-          {t("player.scraping.notFound.badge")}
-        </IconPill>
-        <Title>{t("player.scraping.notFound.title")}</Title>
+        <IconPill icon={icon}>{t("player.scraping.notFound.badge")}</IconPill>
+        <Title>{title}</Title>
         <Paragraph>
           {extensionState === "disallowed" ? (
             <Trans
-              i18nKey="player.scraping.notFound.text"
+              i18nKey="player.scraping.extensionFailure.text"
               components={{
                 bold: (
                   <span className="font-bold" style={{ color: "#cfcfcf" }} />
@@ -87,7 +93,7 @@ export function ScrapeErrorPart(props: ScrapeErrorPartProps) {
             />
           ) : (
             <Trans
-              i18nKey="player.scraping.notFound.text2"
+              i18nKey="player.scraping.notFound.text"
               components={{
                 bold: (
                   <span className="font-bold" style={{ color: "#cfcfcf" }} />
@@ -98,14 +104,6 @@ export function ScrapeErrorPart(props: ScrapeErrorPartProps) {
         </Paragraph>
         <div className="flex gap-3">
           <Button
-            onClick={() => window.location.reload()}
-            theme="secondary"
-            padding="md:px-12 p-2.5"
-            className="mt-6"
-          >
-            {t("player.scraping.notFound.reloadButton")}
-          </Button>
-          <Button
             href="/"
             theme="secondary"
             padding="md:px-12 p-2.5"
@@ -113,15 +111,22 @@ export function ScrapeErrorPart(props: ScrapeErrorPartProps) {
           >
             {t("player.scraping.notFound.homeButton")}
           </Button>
+          <Button
+            onClick={() => {
+              sendPage({
+                page: "PermissionGrant",
+                redirectUrl: window.location.href,
+              });
+            }}
+            theme="purple"
+            padding="md:px-12 p-2.5"
+            className="mt-6"
+          >
+            {extensionState === "unknown"
+              ? t("player.scraping.notFound.detailsButton")
+              : t("player.scraping.extensionFailure.enableExtension")}
+          </Button>
         </div>
-        <Button
-          onClick={() => modal.show()}
-          theme="purple"
-          padding="md:px-12 p-2.5"
-          className="mt-6"
-        >
-          {t("player.scraping.notFound.detailsButton")}
-        </Button>
       </ErrorContainer>
       {error ? (
         <ErrorCardInModal
