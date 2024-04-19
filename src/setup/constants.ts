49111ee21399585229fe01847714af98bb765b86
SolomonChidera <solochidera10@gmail.com>
2024-04-19 15:58:03 +0100

    Reupdated the social links

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,7 +1,7 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "https://movie-web.github.io/links/discord";
-export const GITHUB_LINK = "https://github.com/sussy-code/smov";
-export const TWITTER_LINK = "https://twitter.com/SudoFlix";
+export const GITHUB_LINK = "https://github.com/SolomonChidera/Flixify";
+export const TWITTER_LINK = "https://twitter.com/solomonchidera_";
 export const DONATION_LINK = "https://ko-fi.com/movieweb";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
