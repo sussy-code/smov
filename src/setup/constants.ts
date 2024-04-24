1e0fbd78f46413360896d164030c622da9d032bf
Cooper Ransom <cooperransom08@outlook.com>
2024-04-23 21:18:24 -0400

    test again ...

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -5,4 +5,4 @@ export const TWITTER_LINK = "https://twitter.com/SudoFlix";
 export const DONATION_LINK = "https://ko-fi.com/movieweb";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-// Do it clap?
+// Do it clap??
