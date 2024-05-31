d231e79ad0b808e6b82d1a1fcea5318e48d54a6f
sussy-code <dev@sudo-flix.lol>
2024-05-31 19:15:17 +0000

    rename cat langauge

--- src/assets/languages.ts
+++ src/assets/languages.ts
@@ -3,7 +3,7 @@ import bg from "@/assets/locales/bg.json";
 import bn from "@/assets/locales/bn.json";
 import ca from "@/assets/locales/ca.json";
 import caVl from "@/assets/locales/ca@valencia.json";
-import cat from "@/assets/locales/cat.json";
+import kitty from "@/assets/locales/kitty.json";
 import cs from "@/assets/locales/cs.json";
 import de from "@/assets/locales/de.json";
 import el from "@/assets/locales/el.json";
@@ -62,7 +62,7 @@ export const locales = {
   he,
   sv,
   pirate,
-  cat,
+  kitty,
   uwu,
   minion,
   lv,
