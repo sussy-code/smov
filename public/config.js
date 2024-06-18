44796303a9e03f485d8e04c1bc366249d6bac31b
FMEEE <108198908+FMEEE@users.noreply.github.com>
2024-06-18 13:44:55 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,19 +1,19 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://simple-proxy-zeta.vercel.app,https://glowing-puppy-71e225.netlify.app,https://ver1.fmeee.ovh",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzlkZWYyZDY5ZWFlNDk4ZjJiOTI4MTgyNDdjM2ViMCIsInN1YiI6IjY2MjdmMGJlNjJmMzM1MDE0YmQ4NTFmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h3KpPvkiaz8uNz1bntAKqsPrxG_4UUWaY3kYME6N6m8",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTFiMGE2NjEyMTlhY2QxODVhZTY0ZGQ2MGI4ZTU1ZiIsInN1YiI6IjY1YjNmY2U0MGVkMmFiMDE2Mzg2N2JhOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z434agsWfhKJSIuvHsV9zfFyswty8tvVdN08Ou_7MG0",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
-  VITE_DMCA_EMAIL: null,
+  VITE_DMCA_EMAIL: "fmeee-dmca@mail.ru",
 
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
-  VITE_NORMAL_ROUTER: true,
+  VITE_NORMAL_ROUTER: false,
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: null,
+  VITE_BACKEND_URL: "https://mw-backend.lonelil.ru",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: ""
