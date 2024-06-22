74b25ca3e461c9cf12bcdc72ff67362c624b1f46
Abdullah Khan <85450222+AbdullahDaGoat@users.noreply.github.com>
2024-06-22 19:45:22 -0400

    Update mediaTypes.ts

--- src/utils/mediaTypes.ts
+++ src/utils/mediaTypes.ts
@@ -1,5 +1,4 @@
 export interface MediaItem {
-  seasons: import("c:/Users/huzei/OneDrive/Desktop/Sudo-Flix/src/backend/metadata/types/mw").MWSeasonMeta[];
   id: string;
   title: string;
   year?: number;
