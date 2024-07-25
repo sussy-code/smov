062f143a94f1b21159e61e70873a81b5cb0ac484
FMEEE <108198908+FMEEE@users.noreply.github.com>
2024-07-25 13:18:14 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "https://bare-kessia-fmeee-fc3d6526.koyeb.app.https://glowing-puppy-71e225.netlify.app,https://ver1.fmeee.ovh",
+  VITE_CORS_PROXY_URL: "https://bare-kessia-fmeee-fc3d6526.koyeb.app,https://glowing-puppy-71e225.netlify.app,https://ver1.fmeee.ovh",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTFiMGE2NjEyMTlhY2QxODVhZTY0ZGQ2MGI4ZTU1ZiIsInN1YiI6IjY1YjNmY2U0MGVkMmFiMDE2Mzg2N2JhOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z434agsWfhKJSIuvHsV9zfFyswty8tvVdN08Ou_7MG0",
