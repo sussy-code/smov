29d520275b92fdd96fee3045ff88d8a3024c2007
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-09-25 21:24:31 -0600

    Make it widerrr

--- src/components/layout/WideContainer.tsx
+++ src/components/layout/WideContainer.tsx
@@ -11,8 +11,8 @@ export function WideContainer(props: WideContainerProps) {
     <div
       className={`mx-auto max-w-full px-8 ${
         props.ultraWide
-          ? "w-[1300px] 2xl:w-[2000px] 3xl:w-[2400px] 4xl:w-[2800px]"
-          : "w-[900px] 2xl:w-[1400px] 3xl:w-[1600px] 4xl:w-[1800px]"
+          ? "w-[1300px] xl:w-[18000px] 3xl:w-[2400px] 4xl:w-[2800px]"
+          : "w-[900px] xl:w-[1200px] 3xl:w-[1600px] 4xl:w-[1800px]"
       } ${props.classNames || ""}`}
     >
       {props.children}
