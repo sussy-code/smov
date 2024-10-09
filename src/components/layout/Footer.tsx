38ed72ba8ad67d9a2326be6cadb97b2baf9f893c
icy <76250690+Sidek080@users.noreply.github.com>
2024-10-09 14:54:53 +0200

    sociallinks removed

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -74,16 +74,6 @@ export function Footer() {
           </h3>
           <p className="mt-3">{t("footer.legal.disclaimerText")}</p>
         </div>
-        <div className="flex flex-wrap gap-[0.5rem] -ml-3">
-          <FooterLink icon={Icons.GITHUB} href={conf().GITHUB_LINK}>
-            {t("footer.links.github")}
-          </FooterLink>
-          <FooterLink icon={Icons.DISCORD} href={conf().DISCORD_LINK}>
-            {t("footer.links.discord")}
-          </FooterLink>
-          <FooterLink icon={Icons.TWITTER} href={conf().TWITTER_LINK}>
-            {t("footer.links.twitter")}
-          </FooterLink>
           <div className="inline md:hidden">
             <Dmca />
           </div>
--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,7 +1,7 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://docs.undi.rest/links/discord";
-export const GITHUB_LINK = "https://github.com/sussy-code/smov";
-export const TWITTER_LINK = "https://twitter.com/SudoFlix";
+export const DISCORD_LINK = "";
+export const GITHUB_LINK = "";
+export const TWITTER_LINK = "";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // Do it clap??
