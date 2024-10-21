86aa7564f0c3fc37a2d388263f3bebb9ce332f7e
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-10-20 19:38:12 -0600

    update discord

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://docs.undi.rest/links/discord";
+export const DISCORD_LINK = "https://discord.gg/7z6znYgrTG";
 export const GITHUB_LINK = "https://github.com/sussy-code/smov";
 export const TWITTER_LINK = "https://twitter.com/SudoFlix";
 export const GA_ID = import.meta.env.VITE_GA_ID;
