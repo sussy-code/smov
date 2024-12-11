b7936784dd8cbdeddeb24ea5135b86a0a56cbe3e
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-12-11 11:53:30 -0700

    update captions title for ios

--- src/components/player/internals/VideoContainer.tsx
+++ src/components/player/internals/VideoContainer.tsx
@@ -91,7 +91,7 @@ function VideoElement() {
   if (captionAsTrack && trackObjectUrl && language)
     subtitleTrack = (
       <track
-        label="movie-web"
+        label="P-Stream Captions"
         kind="subtitles"
         srcLang={language}
         src={trackObjectUrl}
